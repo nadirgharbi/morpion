d57e22cf8f0ff353b59c40a46bc6df2ddc93a25e
nadirgharbi <116977046+nadirgharbi@users.noreply.github.com>
2022-12-17 20:33:21 +0100

    Create const.py

---
+++ const.py
@@ -0,0 +1,15 @@
+""" ICI, LES SIMPLES VARIABLES COMME DES ENTIERS, LISTES, STRING, TUPLES... """
+# VARIABLES
+SIZE = WIDTH, HEIGHT = 500,500
+CENTER = 125,225
+DARK = (43,43,43)
+LIGHT = (220,220,220)
+RED = (255,0,0)
+
+# DRAW LINES
+horizontal_line1 = (WIDTH//3) +10
+horizontal_line2 = (WIDTH-horizontal_line1) -10
+vertical_line1 = (WIDTH//3) +10
+vertical_line2 = (WIDTH-vertical_line1)
+
+size_symbol = 50,50
